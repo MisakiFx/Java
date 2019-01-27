8b27ec64c6216e4a8aa035d51d404da09a67f290
MisakiFx <1761607418@qq.com>
2019-01-27 16:02:24 +0800

    2019.1.27

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,10 @@
-public class HelloWorld
+public class HelloWorld//public标识符 class类   HelloWorld类名
 {
-    public static void main(String[] args)
+    public static void main(String[] args)//static静态的 void空的 main入口函数
     {
-        System.out.println("HelloWorld");
+        System.out.println("你好");//system系统 out输出 println()打印并换行
     }
 }
+//java 源程序代码 -》 java字节码文件 -》 运行 -》 java虚拟机 -》 跨平台
+//.java java源代码文件
+//.class java字节码文件 不同平台有不同jvm进行跨平台运行，所以达到平台无关性
