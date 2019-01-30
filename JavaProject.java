3ebaddc7453cd19a06945cc1df1a7527d1076acc
MisakiFx <1761607418@qq.com>
2019-01-30 17:07:37 +0800

    2019.1.30

--- JavaProject.java
+++ JavaProject.java
@@ -1,5 +1,21 @@
+//鎵撳嵃瑙掕壊淇℃伅
 public class JavaProject{
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("索利达尔·群星之怒");//println()打印并换行
+        System.out.println("已绑定\n唯一");
+        System.out.println("远程\t\t\t弓");
+        System.out.println("355 - 524伤害\t\t速度2.70");
+        String brand = "华硕ASUS";//品牌
+        String type = "GTX 1080";//类型
+        int hz = 1569;
+        double length = 29.8;
+        double width = 13.4;
+        double height = 5.25;
+        System.out.println("品牌：" + brand);
+        System.out.println("型号：" + type);
+        System.out.println("核心频率：" + hz + "MHz");
+        System.out.println("长宽高：" + length + "\\" + width + "\\" + height);
+        //未初始化的值不允许打印输出，比C人性化太
+        
     }
 }
