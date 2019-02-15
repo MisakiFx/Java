66bd8d94d5f827bb92c66d0ce8fa815834fc19b3
MisakiFx <1761607418@qq.com>
2019-02-16 03:38:43 +0800

    2019.2.16

---
+++ ArrayDemo.java
@@ -0,0 +1,13 @@
+public class ArrayDemo
+{
+    public static void main(String[] args) 
+    {
+        //数组的定义方法
+        //int[] nums = null;//声明了一个整形数组nums
+        //nums[0] = 9;
+        //这样写会出现异常，就像在C语言中没有给内存空间
+        int[] nums = new int[5];//在堆内存中分配了五个整形空间
+        //数组名就是这一块空间的首地址
+
+    }
+}
