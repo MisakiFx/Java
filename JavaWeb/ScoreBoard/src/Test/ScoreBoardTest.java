22715b7b47fb75b0f372cf7a3c451f4904c6bad8
MisakiFx <1761607418@qq.com>
2019-04-21 09:07:12 +0800

    2019.4.21

--- JavaWeb/ScoreBoard/src/Test/ScoreBoardTest.java
+++ JavaWeb/ScoreBoard/src/Test/ScoreBoardTest.java
@@ -18,15 +18,15 @@ import viewobject.UserViw;
 public class ScoreBoardTest {
 	public static void main(String[] args) throws BusinessException {
 		
-//		MatchProjectServiceImpl matchProjectServiceImpl = new MatchProjectServiceImpl();
-//		List<MatchProjectViw> matchProjectViw = matchProjectServiceImpl.queryMatchProjectList();
-//		for(MatchProjectViw e : matchProjectViw) {
-//			System.out.println(e.getName() + "\t" + e.getTime());
-//		}
-		CollegeServiceImpl collegeServiceImpl = new CollegeServiceImpl();
-		CollegePoint[] collegePoint = collegeServiceImpl.getAllCollegePoint2();
-		for(int i = 0; i < collegePoint.length; i++) {
-			System.out.println(collegePoint[i].getCollegeName() + "\t" + collegePoint[i].getGold() + "\t" + collegePoint[i].getTotal());
+		MatchProjectServiceImpl matchProjectServiceImpl = new MatchProjectServiceImpl();
+		List<MatchProjectViw> matchProjectViw = matchProjectServiceImpl.queryMatchProjectList();
+		for(MatchProjectViw e : matchProjectViw) {
+			System.out.println(e.getName() + "\t" + e.getTime());
 		}
+//		CollegeServiceImpl collegeServiceImpl = new CollegeServiceImpl();
+//		CollegePoint[] collegePoint = collegeServiceImpl.getAllCollegePoint();
+//		for(int i = 0; i < collegePoint.length; i++) {
+//			System.out.println(collegePoint[i].getCollegeName() + "\t" + collegePoint[i].getGold() + "\t" + collegePoint[i].getTotal());
+//		}
 	}
 }
